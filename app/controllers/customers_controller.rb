e9d1cc07a5da5e1676f9bc4d1f35d429efec6ac6
Brent Busby <brent.busby@yahoo.com>
2018-08-02 06:02:36 -0500

    fix indentation, spacing in controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,9 @@
 class CustomersController < ApplicationController
+  def index
+    @customers = gateway.customer.all
+  end
 
-def index
-  @customers = gateway.customer.all
-end
-
-def show
-  @customer = gateway.customer.find(params[:id])
-end
-
+  def show
+    @customer = gateway.customer.find(params[:id])
+  end
 end
